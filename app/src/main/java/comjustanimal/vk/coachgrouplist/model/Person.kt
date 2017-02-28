f3fad1e4b390f7542c1babadf563548b446a6a7b
Alexandr Lopoukhov <alexandrlopoukhov@gmail.com>
2017-02-28 11:39:08 +0300

    person only names - model

--- app/src/main/java/comjustanimal/vk/coachgrouplist/model/Person.kt
+++ app/src/main/java/comjustanimal/vk/coachgrouplist/model/Person.kt
@@ -3,5 +3,4 @@ package comjustanimal.vk.coachgrouplist.model
 /**
  * Created by admin on 2/28/2017.
  */
-class Person {
-}
+data class Person(val firstName: String, val secondName: String, val lastName: String)
